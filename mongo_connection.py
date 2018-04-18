728e3cb2adb731ac08cabcdbfa6ddf93e42e5729
chriswang2468 <chriswang2468@gmail.com>
2018-04-18 19:32:50 +0800

    initialize the project

---
+++ mongo_connection.py
@@ -0,0 +1,25 @@
+from sshtunnel import SSHTunnelForwarder
+import pymongo
+import pprint
+
+def mongoConnection():
+	MONGO_HOST = "13.58.236.43"
+	MONGO_DB = "Quant_database"
+	MONGO_USER = "admin"
+	MONGO_PASS = "admin"
+	server = SSHTunnelForwarder(
+	    MONGO_HOST,
+	    ssh_username='chris',
+	    ssh_password='wang0408',
+	    remote_bind_address=('127.0.0.1', 27017)
+	)
+
+	server.start()
+
+	client = pymongo.MongoClient('127.0.0.1', server.local_bind_port) # server.local_bind_port is assigned local port
+	db = client[MONGO_DB]
+	pprint.pprint(db.collection_names())
+
+	server.stop()
+
+def setServer():
